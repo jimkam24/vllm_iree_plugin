10748a88b60518651ca3e79027eebb3122895d56
jimkam24 <dimkampanakis@gmail.com>
2026-06-21 15:20:28 +0000

    before trying changes for compile

--- vllm_test/benchmark_prefill_decode.py
+++ vllm_test/benchmark_prefill_decode.py
@@ -73,7 +73,7 @@ if CONFIG == "1":
         model="meta-llama/Llama-3.2-1B",
         dtype="float32",
         max_model_len=512,
-        enforce_eager=False,
+        enforce_eager=True,
         pipeline_parallel_size=2,
         disable_log_stats=True,
     )
@@ -132,7 +132,7 @@ else:
         model="meta-llama/Llama-3.2-1B",
         dtype="float32",
         max_model_len=512,
-        enforce_eager=False,
+        enforce_eager=True,
         gpu_memory_utilization=0.5,
         distributed_executor_backend=(
             "vllm_plugin.executor.hybrid_executor.HybridExecutor"
